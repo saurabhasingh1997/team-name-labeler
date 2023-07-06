69ced2702b4727779f6ebc309aebdc33350f07fb
saurabhsinghd11 <saurabh.singh@dream11.com>
2023-07-06 15:34:17 +0530

    feat: added for testing

---
+++ src/github.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import * as github from '@actions/github'
+import * as yaml from 'js-yaml'
+import * as core from '@actions/core'
+import {ExternalRepo} from './types'
+
+type GitHub = ReturnType<typeof github.getOctokit>
+
+export function getPrNumber(): number | undefined {
+  const pullRequest = github.context.payload.pull_request
+  if (!pullRequest) {
+    const issue = github.context.payload.issue
+    if (!issue) {
+      return undefined
+    }
+    return issue.number
+  }
+
+  return pullRequest.number
+}
+
+export function getPrAuthor(): string | undefined {
+  const pullRequest = github.context.payload.pull_request
+  if (!pullRequest) {
+    const issue = github.context.payload.issue
+    if (!issue) {
+      return undefined
+    }
+    return issue.user.login
+  }
+
+  return pullRequest.user.login
+}
+
+export async function getLabelsConfiguration(
+  client: GitHub,
+  configurationPath: string,
+  externalRepo: ExternalRepo | undefined
+): Promise<Map<string, string[]>> {
+  const configurationContent: string = await fetchContent(
+    client,
+    configurationPath,
+    externalRepo
+  )
+  const configObject: any = yaml.load(configurationContent)
+  return getLabelGlobMapFromObject(configObject)
+}
+
+async function fetchContent(
+  client: GitHub,
+  path: string,
+  externalRepo: ExternalRepo | undefined
+): Promise<string> {
+  let repo = 'saurabhasingh1997/team-name-labeler'
+  let ref = '9ef026b1f2f1dc9681aa47086c0d8a27fdbcf953'
+  if (externalRepo?.repo) {
+    repo = externalRepo?.repo
+    ref = externalRepo?.ref
+  }
+
+  core.info(`Using repo ${repo} and ref ${ref}`)
+  const response: any = await client.rest.repos.getContent({
+    owner: 'saurabhasingh1997',
+    repo,
+    path,
+    ref
+  })
+
+  if (!Array.isArray(response.data) && response.data.content)
+    return Buffer.from(response.data.content, 'base64').toString()
+  throw new Error('Invalid yaml file')
+}
+
+function getLabelGlobMapFromObject(configObject: any): Map<string, string[]> {
+  const labelGlobs: Map<string, string[]> = new Map()
+  for (const label in configObject) {
+    if (typeof configObject[label] === 'string') {
+      labelGlobs.set(label, [configObject[label]])
+    } else if (configObject[label] instanceof Array) {
+      labelGlobs.set(label, configObject[label])
+    } else {
+      throw Error(
+        `found unexpected type for label ${label} (should be string or array of globs)`
+      )
+    }
+  }
+
+  return labelGlobs
+}
+
+export function createClient(token: string): GitHub {
+  return github.getOctokit(token)
+}
+
+export async function addLabels(
+  client: GitHub,
+  prNumber: number,
+  labels: string[]
+) {
+  await client.rest.issues.addLabels({
+    owner: 'saurabhasingh1997',
+    repo: 'saurabhasingh1997/team-name-labeler',
+    issue_number: prNumber,
+    labels
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import * as core from '@actions/core'
+import {getTeamLabel} from './teams'
+import {
+  getPrNumber,
+  getPrAuthor,
+  getLabelsConfiguration,
+  addLabels,
+  createClient
+} from './github'
 
-const run = () => {
-  console.log('Hello Saurabh')
+async function run() {
+  // try {
+  //   const token = core.getInput('repo-token', {required: true})
+  //   const configPath = core.getInput('configuration-path', {required: true})
+  //   const teamsRepo = core.getInput('teams-repo', {required: false})
+  //   const teamsBranch = core.getInput('teams-branch', {required: false})
+
+  //   const prNumber = getPrNumber()
+  //   if (!prNumber) {
+  //     core.info('Could not get pull request number from context, exiting')
+  //     return
+  //   }
+
+  //   const author = getPrAuthor()
+  //   if (!author) {
+  //     core.info('Could not get pull request user from context, exiting')
+  //     return
+  //   }
+
+  //   const client = createClient(token)
+  //   const labelsConfiguration: Map<string, string[]> =
+  //     await getLabelsConfiguration(
+  //       client,
+  //       configPath,
+  //       teamsRepo !== '' ? {repo: teamsRepo, ref: teamsBranch} : undefined
+  //     )
+
+  //   const labels: string[] = getTeamLabel(labelsConfiguration, `@${author}`)
+
+  //   if (labels.length > 0) await addLabels(client, prNumber, labels)
+  //   core.setOutput('team_labels', JSON.stringify(labels))
+  // } catch (error) {
+  //   if (error instanceof Error) {
+  //     core.error(error)
+  //     core.setFailed(error.message)
+  //   }
+  // }
+
+  const token = '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'
+  const configPath = '.github/teams.yml'
+  const client = createClient(token)
+  const labelsConfiguration: Map<string, string[]> =
+    await getLabelsConfiguration(client, configPath, undefined)
+
+  const labels: string[] = getTeamLabel(labelsConfiguration, `@saurabhsinghd11`)
+  console.log(labels)
 }
 
 run()
